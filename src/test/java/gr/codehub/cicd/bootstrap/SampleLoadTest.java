4eb9f03eb7a782138c101d7c3eda1fab7ee4c71e
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-12-02 22:26:09 +0200

    alternative SampleLoadTest for test in github

--- src/test/java/gr/codehub/cicd/bootstrap/SampleLoadTest.java
+++ src/test/java/gr/codehub/cicd/bootstrap/SampleLoadTest.java
@@ -1,39 +1,46 @@
 package gr.codehub.cicd.bootstrap;
 
 import gr.codehub.cicd.service.DatabaseService;
-import gr.codehub.cicd.service.DatabaseServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
 
 
-//@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class)
 public class SampleLoadTest {
-//
-//    @Mock
-//    private DatabaseService dbServiceMock;  // Mocking the DatabaseService interface
-//
+
+    @Mock
+    private DatabaseService dbServiceMock;  // Mocking the DatabaseService interface
+
 //    @InjectMocks
 //    private SampleLoad sampleLoad;  // This will inject the mock into SampleLoad
-//
-//    @BeforeEach
-//    public void setup() {
-//        // MockitoAnnotations.openMocks(this);  // Initialize mocks, superseded by ExtendWith
-//    }
-//
-//    @Test
-//    // TODO update tests
-//    public void testLoadSampleData() {
-//        // Call the method that should invoke clearDatabase
-//        sampleLoad.loadSampleData();
-//
-//        // Verify that clearDatabase() was called
-//        //verify(dbServiceMock, times(1)).clearDatabase();
-//        verify(dbServiceMock).clearDatabase();
-//
-//        // Verify other interactions if necessary
-//        // For example, verify that saveEmployee was called for each employee
-//        // verify(dbServiceMock, atLeastOnce()).saveEmployee(any());
-//        verify(dbServiceMock, atLeast(5)).saveEmployee(any());
-//    }
+
+    @BeforeEach
+    public void setup() {
+        // MockitoAnnotations.openMocks(this);  // Initialize mocks, superseded by ExtendWith
+    }
+
+    @Test
+    // TODO update tests
+    public void testLoadSampleData() {
+        // Call the method that should invoke clearDatabase
+        SampleLoad sampleLoad = new SampleLoad(dbServiceMock);
+        sampleLoad.loadSampleData();
+
+        // Verify that clearDatabase() was called
+        //verify(dbServiceMock, times(1)).clearDatabase();
+        verify(dbServiceMock).clearDatabase();
+
+        // Verify other interactions if necessary
+        // For example, verify that saveEmployee was called for each employee
+        // verify(dbServiceMock, atLeastOnce()).saveEmployee(any());
+        verify(dbServiceMock, atLeast(5)).saveEmployee(any());
+    }
 }
